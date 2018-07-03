5ccf99ab371d2128f46e3c8d1ac7979360e54ab9
Isaac Owomugisha <archimedesisaac@gmail.com>
2018-07-03 06:09:53 +0300

    Add verify password method

--- app/views.py
+++ app/views.py
@@ -51,10 +51,14 @@ def login(username, password):
 
 
 @auth.verify_password
-def verify_password(username, password):
-    user = User.get_user(username)
-    if not user or not user.verify_password(password):
-        return False
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.get_user(username=username_or_token)
+        if not user or not user.verify_password(password):
+            return False
 
     g.user = user
     return True
