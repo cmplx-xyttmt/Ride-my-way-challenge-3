e585f54bbb4abb84d4cfd9a84a2bf22ce0ad7cb8
Isaac Owomugisha <archimedesisaac@gmail.com>
2018-07-17 18:37:40 +0300

    Add flask_cors to enable Cross Origin Resource Sharing (CORS)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import app_config
+from flask_cors import CORS
 
 
 def create_app(config_name):
     application = Flask(__name__)
+    CORS(application)
     application.config.from_object(app_config[config_name])
 
     return application
