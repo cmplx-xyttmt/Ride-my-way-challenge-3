c32075254c312b2ac7efc46ab35fb93e82ebc68e
Isaac Owomugisha <archimedesisaac@gmail.com>
2018-07-23 19:19:31 +0300

    Add accept/reject helper method

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -2,8 +2,8 @@
 This file contains helper functions to be used in the views.py
 It helps keep the codebase manageable
 """
-from app.models import User, Ride, Request
-from flask import request, abort, jsonify, make_response
+from app.models import User, Request
+from flask import abort, jsonify, make_response
 
 
 def sign_up_user(username, password, email):
@@ -68,3 +68,31 @@ def return_requests(ride_id, ride, username):
         abort(401,
               'You are not authorized to view these '
               'ride requests because you did not create this ride offer.')
+
+
+def accept_or_reject(request_id, data):
+    if 'decision' not in data:
+        abort(400,
+              'Make sure you have a decision key in your request')
+    decision = data['decision']
+    if not (decision == "accept" or decision == "reject"):
+        abort(400,
+              'Specify your decision by setting the '
+              'decision key to accept or reject'
+              )
+
+    success = Request.accept_reject_ride_request(decision, request_id)
+    if success:
+        if decision == 'accept':
+            message = 'You have accepted this ride request'
+        else:
+            message = 'You have rejected this ride request'
+
+        response = {
+            'status': message
+        }
+    else:
+        response = {
+            'status': 'Failed to accept or reject the ride request'
+        }
+    return make_response(jsonify(response)), 200
